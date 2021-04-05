82f67e0935c33ce852fcbd8a07ed7c76dad928d0
jbuider1993 <jbuider1993@gmail.com>
2021-04-04 20:02:55 -0400

    Test code change

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,4 @@
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (1, 'Jie Liu','john_william@gmail.com');
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (2, 'Mike Daniel','mike_daniel@yahoo.co.in');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (1, 'Jie Liu','jbuilder1993@gmail.com');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (2, 'Xiao Ying Li','mike_daniel@yahoo.co.in');
 INSERT INTO USERS (ID, NAME, EMAILID) VALUES (3, 'David Lawrance','david_law@gmail.com');
+INSERT INTO USERS (ID, NAME, EMAILID) VALUES (4, 'Cjang yu Liu','david_law@gmail.com');
