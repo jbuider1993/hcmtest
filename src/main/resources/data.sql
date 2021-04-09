240590fccda02a8ccfc860e64dfa46b7cc9abd99
jbuider1993 <jbuider1993@gmail.com>
2021-04-09 16:02:17 -0400

    add mysql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (1, 'Jie Liu','jbuilder1993@gmail.com');
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (2, 'Xiao Ying Li','mike_daniel@yahoo.co.in');
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (3, 'David Lawrance','david_law@gmail.com');
-INSERT INTO USERS (ID, NAME, EMAILID) VALUES (4, 'Cjang yu Liu','david_law@gmail.com');
+INSERT INTO workpower.USERS (ID, NAME, EMAILID) VALUES (1, 'Jie Liu','jbuilder1993@gmail.com');
+INSERT INTO workpower.USERS (ID, NAME, EMAILID) VALUES (2, 'Xiao Ying Li','mike_daniel@yahoo.co.in');
+INSERT INTO workpower.USERS (ID, NAME, EMAILID) VALUES (3, 'David Lawrance','david_law@gmail.com');
+INSERT INTO workpower.USERS (ID, NAME, EMAILID) VALUES (4, 'Cjang yu Liu','david_law@gmail.com');
