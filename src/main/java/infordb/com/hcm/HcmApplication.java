e5ad6b6a4812c479b0cc59afa6888ce1fb2944ff
jbuider1993 <jbuider1993@gmail.com>
2021-04-12 13:13:07 -0400

    update the code

--- src/main/java/infordb/com/hcm/HcmApplication.java
+++ src/main/java/infordb/com/hcm/HcmApplication.java
@@ -2,12 +2,29 @@ package infordb.com.hcm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableSwagger2
 public class HcmApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HcmApplication.class, args);
 	}
 
+	@Bean
+	public Docket swaggerPersonApi10() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("infordb.com.hcm.controller"))
+				.paths(PathSelectors.any())
+				.build()
+				.apiInfo(new ApiInfoBuilder().version("1.0").title("Employee API").description("Documentation Employee API v1.0").build());
+	}
 }
